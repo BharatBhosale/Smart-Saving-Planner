cccab203c6124afb0f7dbd552f2ecc93933e136e
Bharat Bhosale <bharatvbhosale96@gmail.com>
2025-11-18 23:18:50 +0530

    setup

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <>
+      <div>index</div>
+      <button>click me</button>
+    </>
+  );
+};
+
+export default Button;
---
+++ frontend/src/components/SavingPlaner/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SavingPlaner = () => {
+  return (
+    <div className="px-10 flex justify-between bg-[#50d71e] py-5">
+      <div>Smart Saving Planner</div>
+      <div>
+        <button>login</button>
+      </div>
+    </div>
+  );
+};
+
+export default SavingPlaner;
