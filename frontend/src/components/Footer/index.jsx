19a90e3787bfa15831dc1c4aecc324788453a8ce
shrutifugate <fugateshruti@gmail.com>
2025-11-26 16:25:05 +0530

    footer update

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Style.css";
+
+
+
+const Footer = () => {
+  return (
+ 
+<>
+<h1 className="h1">hi</h1>
+<h2>hello</h2>
+</>
+
+
+
+  
+  );
+};
+
+export default Footer;
